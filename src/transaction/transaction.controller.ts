0863fb2dab6ca8e70ee95d9ef5213af25033b1c1
Oleksandr <ace.7557@yahoo.com>
2024-11-28 07:47:50 +0200

    Ver.05

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+
+import { TransactionService } from './transaction.service';
+import { CreateTransactionDto } from './dto/transaction.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('transaction')
-export class TransactionController {}
+export class TransactionController {
+  constructor(private readonly transactionService: TransactionService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  @UseGuards(JwtAuthGuard)
+  create(@Body() createTransactionDto: CreateTransactionDto, @Req() req) {
+    return this.transactionService.create(createTransactionDto, +req.user.id);
+  }
+}
--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { CreateTransactionDto } from './dto/transaction.dto';
+import { Transaction } from './entities/transaction.entity';
 
 @Injectable()
-export class TransactionService {}
+export class TransactionService {
+  constructor(
+    @InjectRepository(Transaction)
+    private readonly transactionRepository: Repository<Transaction>,
+  ) {}
+
+  async create(createTransactionDto: CreateTransactionDto, id: number) {
+    const newTransaction = {
+      title: createTransactionDto.title,
+      amount: createTransactionDto.amount,
+      type: createTransactionDto.type,
+      category: { id: +createTransactionDto.category },
+      user: { id: id },
+    };
+    if (!newTransaction) {
+      throw new BadRequestException('Transaction not created');
+    } else {
+      await this.transactionRepository.save(newTransaction);
+      return 'Transaction was added';
+    }
+  }
+}
+
+/*
+{
+  "title": "Car",
+  "amount": 15000,
+  "type": "expenses",
+  "category": 2;
+}
+*/
